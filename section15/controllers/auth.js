5aa156b861cc63e683a94a9a91f0fd07f82bbafa
YGS <tig03027@naver.com>
2025-03-30 03:04:30 +0900

    section 15 complete

---
+++ section15/controllers/auth.js
@@ -0,0 +1,103 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMassage: message,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMassage: message,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Invalid email or password");
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              // req.session.save = session이 저장된 것을 확인하고 그 후 콜백함수를 호출 하는 함수
+              // 보통은 사용할 필요가 없지만 redirect와 같이 서버와 데이터베이스에 세션이 저장되기도 전에 실행될 수 있는 함수와 함께 사용한다
+              res.redirect("/");
+            });
+          }
+          req.flash("error", "Invalid email or password");
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        req.flash(
+          "error",
+          "E-mail exists already, please pick a different one"
+        );
+        return res.redirect("/signup");
+      }
+      // user가 이미 존재할 시 res.redirect로 페이지를 이동하지만 다음의 promise.then은 그대로 실행되기 때문에 hash에 바로 then을 붙인다.
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
